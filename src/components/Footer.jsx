cfde73fbfb3334a7c36f728d6712ef482df05dea
rajchuahn-12 <rajchuahan25112004@gmail.com>
2025-07-22 16:42:53 +0530

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Footer() {
-  return <footer>© 2025 Cafe App. All rights reserved.</footer>;
+  return <footer>© 2025 Raj Chauhan. All rights reserved.</footer>;
 }
