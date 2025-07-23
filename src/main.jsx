7be55eb9f96e58789b3bea4f2cd2808f07ccc2b6
whyshivansh <shivanshsaxena1310@gnail.com>
2025-07-23 14:22:09 +0530

    push

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import './components/App.css';
+import { registerSW } from 'virtual:pwa-register';
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content available. Reload?")) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log("App is ready to work offline.");
+  }
+});
 
 
 createRoot(document.getElementById("root")).render(<App />);
